1699ce87c95d666e91ea46dee7e9f7bdb69c787c
Gauri Chaudhary <35339147+Gaurichaudhary@users.noreply.github.com>
2018-02-04 14:43:16 -0800

    Create holiday.c

---
+++ holiday.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+ char str1[10]= "Sunday", str2[10] = "Saturday",str[10];
+ int a;
+   
+scanf("%s",&str);
+
+if(strcmp(str1, str)==0||strcmp(str2,str)==0)
+{
+    printf("yes");
+}
+   else 
+{
+printf("no");
+}
+
+}
